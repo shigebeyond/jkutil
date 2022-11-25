c091e53539c41d9d13bb072b736c9073d465df6f
shijianhang <3076679995@qq.com>
2022-11-25 15:33:20 +0800

    去掉无用代码

--- src/main/kotlin/net/jkcode/jkutil/common/_Logger.kt
+++ src/main/kotlin/net/jkcode/jkutil/common/_Logger.kt
@@ -13,17 +13,6 @@ val esLogger = switcher.getLogger("net.jkcode.jkmvc.db")
 // http的日志
 val httpLogger = switcher.getLogger("net.jkcode.jkmvc.http")
 
-// php异常类
-val phpExceptionClazz: Class<*>? by lazy {
-    try{
-        Class.forName("php.runtime.ext.java.JavaException")
-    }catch(e: Exception){
-        null
-    }
-}
-
-val throwableField = phpExceptionClazz?.getAccessibleField("throwable")
-
 /**
  * 对异常打日志, 带颜色
  * @param msg
@@ -31,11 +20,5 @@ val throwableField = phpExceptionClazz?.getAccessibleField("throwable")
  */
 public fun Logger.errorColor(msg: String, e: Throwable) {
     val msg = ColorFormatter.applyTextColor(msg, 31) // 红色
-    var ex = e
-    // 如果是php包装的异常, 则输出java原生异常
-    //if(e.cause is JavaException)
-    //  ex = (e.cause as JavaException).throwable
-    if(phpExceptionClazz != null && phpExceptionClazz!!.isInstance(e.cause))
-        ex = throwableField!!.get(e.cause) as Throwable
-    this.error(msg, ex)
+    this.error(msg, e)
 }
